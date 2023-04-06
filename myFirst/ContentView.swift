9b19bf6c7000d703b527af5e253f12db9c34d543
st0g1e <st0g1e@yahoo.com>
2023-04-06 12:45:37 +0800

    Initial Commit

---
+++ myFirst/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  myFirst
+//
+//  Created by st0g1e on 4/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myFirst/myFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstApp.swift
+//  myFirst
+//
+//  Created by st0g1e on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct myFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
